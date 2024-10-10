86f71610a8987989dbb3f21ff786d2c2781faf00
Juan Roldan <juanroldan1989@gmail.com>
2024-10-10 21:56:30 +0200

    Docker Entrypoint script adjusted
    - `uploads` folder setup
    - `docker` folder added to contain Dockerfile improvements sequence

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# Check if the uploads directory exists, if not, initialize it
+if [ ! -d "uploads" ]; then
+  echo "Uploads folder not found, initializing..."
+  mkdir uploads
+fi
+
 # Check if the migrations directory exists, if not, initialize it
 if [ ! -d "migrations" ]; then
-    echo "Migrations folder not found, initializing..."
-    flask db init
-    flask db migrate -m "Initial migration"
+  echo "Migrations folder not found, initializing..."
+  flask db init
+  flask db migrate -m "Initial migration"
 fi
 
 # Apply any pending migrations
